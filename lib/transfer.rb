b8f2832d49d9bfe6cd1a228e632dcc24707015db
madhuv2002 <madhuv2002@gmail.com>
2018-07-13 19:09:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,8 +13,15 @@ class Transfer
     sender.valid? && receiver.valid?
   end
     
-  def execute_transaction
-    if sender.valid?
+    def execute_transaction
+     if valid? && sender.balance > amount && self.status == "pending"
+       sender.balance -= amount
+       receiver.balance += amount
+       self.status = "complete"
+     else
+       reject_transfer
+     end
+   end
   
 end
 
